f8aff3701eaef25a1a6d4e28b4973a91da7462d3
Christian Abdullahad <christian.abdullahad@hotmail.de>
2021-12-01 06:38:50 +0100

    small changes to 01

--- 01.rb
+++ 01.rb
@@ -1,38 +1,26 @@
 lines = File.readlines('01.txt').map(&:to_i)
 
-
 def count(lines)
-    last = :nan
     count = 0
-    lines.each do |l|
-        if last != :nan and last < l
-            count += 1
-        end
-        last = l
+    lines.each_cons(2) do |l, k|
+        count += 1 if l < k
     end
     return count
 end
-
-
-# part 1
 
 def part01(lines)
     puts "count: #{count(lines)}"
 end
 
-# part 2
-
 def part02(lines)
     numbers = []
 
-    (0...lines.length - 2).each do |i|
-        numbers << lines[i] + lines[i+1] + lines[i+2]
+    lines.each_cons(3) do |i, j, k|
+        numbers << i + j + k
     end
 
     puts "count: #{count(numbers)}"
 end
-
-
 
 part01(lines)
 part02(lines)
